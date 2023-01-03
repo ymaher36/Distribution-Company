4fc58baf847db619b22be30912f424f39d75d549
tanzeem37 <tanzeem37@manzoma>
2023-01-03 10:07:21 +0200

    static modifications

--- static/navbar.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-    jQuery(document).ready(function () {
-        $(".dropdown").hover(function () {
-            $('.dropdown-menu', this).stop().fadeIn("fast");
-        }, function () {
-            $('.dropdown-menu', this).stop().fadeOut("fast");
-        });
-    });
-})
--- tanzeem/asgi.py
+++ tanzeem/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for distributioncompany project.
+ASGI config for tanzeem project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'distributioncompany.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tanzeem.settings')
 
 application = get_asgi_application()
